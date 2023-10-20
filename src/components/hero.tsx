05d6dbae7fda46c2648fb4eae2cb436f55f43415
Your Name <afwcole@gmail.com>
2023-10-20 19:00:03 +0000

    Hero Copy draft

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,7 +1,12 @@
+"use client"
+
 import VideoThumb from '../../public/images/hero-image.png'
 import ModalVideo from '@/components/modal-video'
+import { useState } from 'react'
 
 export default function Hero() {
+  const [modalOpen, setModalOpen] = useState(false);
+
   return (
     <section className="relative">
 
@@ -29,15 +34,32 @@ export default function Hero() {
 
           {/* Section header */}
           <div className="text-center pb-12 md:pb-16">
-            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Make your website <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">wonderful</span></h1>
+            <h1 className="text-5xl md:text-7xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">
+              Send Ripple to any phone. <br />
+              <span className='text-6xl text-gray-500'> No Internet. No Smartphone. <br /></span>
+              <span className="text-7xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
+                No Problem!
+              </span>
+            </h1>
             <div className="max-w-3xl mx-auto">
-              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.</p>
+              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">
+                Financial empowerment and inclusion for over a billion people without internet access or quality smartphones. Create accounts, send XRP and more!
+              </p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
-                  <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Start free trial</a>
+                  <a
+                    className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0"
+                    href="#0"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setModalOpen(true);
+                    }}
+                  >
+                    Watch Explainer
+                  </a>
                 </div>
                 <div>
-                  <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Learn more</a>
+                  <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Try Simulator</a>
                 </div>
               </div>
             </div>
@@ -45,6 +67,8 @@ export default function Hero() {
 
           {/* Hero image */}
           <ModalVideo
+            modalOpen={modalOpen} 
+            setModalOpen={setModalOpen}
             thumb={VideoThumb}
             thumbWidth={768}
             thumbHeight={432}
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -28,8 +28,9 @@ export default function Header() {
         <div className="flex items-center justify-between h-16 md:h-20">
 
           {/* Site branding */}
-          <div className="shrink-0 mr-4">
+          <div className="shrink-0 mr-4 flex items-center space-x-3">
             <Logo />
+            <h1 className='font-bold text-lg bg-clip-text text-transparent bg-blue-500'>Ripple Mobile</h1>
           </div>
 
           {/* Desktop navigation */}
@@ -37,11 +38,8 @@ export default function Header() {
             {/* Desktop sign in links */}
             <ul className="flex grow justify-end flex-wrap items-center">
               <li>
-                <Link href="/signin" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Sign in</Link>
-              </li>
-              <li>
                 <Link href="/signup" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
-                  <span>Sign up</span>
+                  <span>Try Simulator</span>
                   <svg className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                     <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
                   </svg>
