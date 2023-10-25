803a119b93742219a287cda4dea86c94c6264245
eliblurr <elvissegbawu@gmail.com>
2023-10-25 12:00:41 +0000

    rename env variable

--- src/apis/ussd.ts
+++ src/apis/ussd.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import { PostPayload, ResponsePayload } from '@/types/types';
 
-const apiUrl: string= process.env.API_BASE_URL || 'http://localhost:8000/incoming-ussd-request/';
+const apiUrl: string= process.env.API_URL || 'http://localhost:8000/incoming-ussd-request/';
 
 if (!apiUrl) {
   console.error('Environment variable is not defined or empty.');
 } else {
-  console.log('API_BASE_URL:', apiUrl);
+  console.log('API_URL:', apiUrl);
 }
 
 export const post = async (data: PostPayload) => {
