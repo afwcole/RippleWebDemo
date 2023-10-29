b10b5a1a496b8b96fdc97d3bd236fba77dc18c09
eliblurr <elvissegbawu@gmail.com>
2023-10-29 14:05:21 +0000

    increase carousel stay time

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -41,12 +41,12 @@ export default function Hero() {
             {/* Carousel Section */}
             <div className='relative w-full pt-20 pb-10'>
               {/* Carousel */}
-              <div id="default-carousel" className="absolute inset-0 -z-[2] bg-black bg-opacity-70" data-carousel="slide">
+              <div id="default-carousel" className="absolute inset-0 -z-[2] bg-black bg-opacity-70" data-carousel="slide" data-carousel-interval="7000">
                 <div className="relative overflow-hidden roundedLg h-full">
-                  <div className="hidden duration-[800ms] ease-linear" data-carousel-item>
+                  <div className="hidden duration-[650ms] ease-linear" data-carousel-item>
                     <Image src={Hero2} className="absolute block w-full h-full object-cover -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 brightness-[0.4]" alt="..."/>
                   </div>
-                  <div className="hidden duration-[800ms] ease-linear" data-carousel-item>
+                  <div className="hidden duration-[650ms] ease-linear" data-carousel-item>
                     <Image src={Hero1} className="absolute block w-full h-full object-cover -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 brightness-[0.4]" alt="..."/>
                   </div>
                 </div>
