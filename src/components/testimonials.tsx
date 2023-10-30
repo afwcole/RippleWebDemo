aa50305586bfad7c5f0d7b4029a16d4eea6046ef
eliblurr <elvissegbawu@gmail.com>
2023-10-30 15:21:37 +0000

    align testimonial card

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -37,31 +37,35 @@ export default function Testimonials() {
           <div className="max-w-3xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-4" data-aos="zoom-y-out">
             {/* Testimonial - 1 */}
             <div className="relative flex col-span-1 items-start border-2 border-gray-200 rounded bg-white">
-              <div className="text-center px-12 py-8 pt-20 mx-4 md:mx-0">
+              <div className="text-center px-12 py-8 pt-20 mx-4 md:mx-0 flex flex-col justify-between h-full">
                 <div className="absolute top-0 -mt-8 left-1/2 transform -translate-x-1/2">
                   <Image className="relative rounded-full w-20 h-20 object-cover" src={fredImage} width={96} height={96} alt="Testimonial 01" />
                 </div>
                 <blockquote className="text-xl font-medium mb-4">
                   {"\"As a freelancer, receiving payment from international clients can be a hassle. But with Ripple Mobile I can get paid for my gigs and send money home to my mother in our village, all with one platform. She loves the simplicity of Ripple Mobile&apos;s USSD on her basic phone. It allows me to get paid by my clients and provide for my family no matter the distance.\""}  </blockquote>
-                <cite className="block font-bold text-lg not-italic mb-1">Fred</cite>
-                <div className="text-gray-600">
-                  <span>Freelancer</span>
+                <div>
+                  <cite className="block font-bold text-lg not-italic mb-1">Fred</cite>
+                  <div className="text-gray-600">
+                    <span>Freelancer</span>
+                  </div>
                 </div>
               </div>
             </div>
 
             {/* Testimonial - 2 */}
             <div className="relative flex col-span-1 items-start border-2 border-gray-200 rounded bg-white">
-              <div className="text-center px-12 py-8 pt-20 mx-4 md:mx-0">
+              <div className="text-center px-12 py-8 pt-20 mx-4 md:mx-0 flex flex-col justify-between h-full">
                 <div className="absolute top-0 -mt-8 left-1/2 transform -translate-x-1/2">
                   <Image className="relative rounded-full w-20 h-20 object-cover" src={maryImage} width={96} height={96} alt="Testimonial 02" />
                 </div>
                 <blockquote className="text-xl font-medium mb-4">
                   {"\"Ripple Mobile's POS system was a revelation for my shop. It's simple enough for someone like me who isn't tech-savvy, and my customers love how fast they can check out. And when it's time to restock, paying my suppliers from around the world feels so so easy. \""}
                 </blockquote>
-                <cite className="block font-bold text-lg not-italic mb-1">Mary</cite>
-                <div className="text-gray-600">
-                  <span>Marketwoman</span>
+                <div>
+                  <cite className="block font-bold text-lg not-italic mb-1">Mary</cite>
+                  <div className="text-gray-600">
+                    <span>Marketwoman</span>
+                  </div> 
                 </div>
               </div>
             </div>
